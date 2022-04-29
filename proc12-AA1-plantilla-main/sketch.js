b9dd3b34848198872274fc8cf44c13002eeb6ca3
JuanPabloCervMur <102774869+JuanPabloCervMur@users.noreply.github.com>
2022-04-29 12:18:21 -0500

    Add files via upload

---
+++ proc12-AA1-plantilla-main/sketch.js
@@ -0,0 +1,21 @@
+var marks = [35,38,42,45,43,34,46,41,48,32];
+  console.log (marks.length)
+  
+//console.log (marks [0] && marks [1])
+for(var mar =0; mar < marks.length; mar ++ ){
+  console.log (marks[mar])
+  }
+
+function setup() 
+{
+  createCanvas(400,400);
+
+}
+
+function draw() 
+{
+  background(30);
+}
+
+ 
+
